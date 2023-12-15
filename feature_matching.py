109d48cf0623d5212cbd0a597d40c48ab6d0aa1a
Artur Alves <arturalves.ca@gmail.com>
2023-12-14 23:10:45 -0300

    update classic methods for matching

--- feature_matching.py
+++ feature_matching.py
@@ -1,11 +1,20 @@
 import cv2
-import numpy as np
-import matplotlib.pyplot as plt
+import argparse
+from pathlib import Path
 
-def bf_matching(template_path, bkg_path, ratio_test = True):
+""" 
+Draw matching for a single template images and a background image using SIFT algorithm
+
+"""
+def bf_matching(template_path : str, bkg_path : str, ratio_test : bool = True) -> None:
+    # use colored images for printing
+    colored_template_img = cv2.imread(template_path) 
+    colored_bkg_img = cv2.imread(bkg_path)
+
+    # use gray images for analysis
     template_img = cv2.imread(template_path, cv2.IMREAD_GRAYSCALE) 
-    bkg_img = cv2.imread(bkg_path, cv2.IMREAD_GRAYSCALE)
-
+    bkg_img = cv2.imread(bkg_path, cv2.IMREAD_GRAYSCALE)    
+    
     sift = cv2.SIFT_create()
 
     kp_template, des_template = sift.detectAndCompute(template_img, None)
@@ -13,23 +22,39 @@ def bf_matching(template_path, bkg_path, ratio_test = True):
     
     bf = cv2.BFMatcher() 
     if ratio_test:  # Lowe's ratio test --> (short explanation: https://stackoverflow.com/questions/51197091/how-does-the-lowes-ratio-test-work)
-        matches = bf.knnMatch(des_template, des_bkg, k = 2) # find two best matches (closest) for each keypoint
+        matches = bf.knnMatch(des_template, des_bkg, k = 2) # find two best matches for each keypoint
         good = []
-        ctx = 0.8 # adjust this value
+        ctx = 0.8
         for m,n in matches:
             if m.distance < ctx * n.distance:
                 good.append([m])
-        res_img = cv2.drawMatchesKnn(template_img, kp_template, bkg_img, kp_bkg, good, None, flags = cv2.DRAW_MATCHES_FLAGS_NOT_DRAW_SINGLE_POINTS)
+        res_img = cv2.drawMatchesKnn(colored_template_img, kp_template, colored_bkg_img, kp_bkg, good, None, flags = cv2.DRAW_MATCHES_FLAGS_NOT_DRAW_SINGLE_POINTS)
     else: 
-        matches = bf.match(des_template, des_bkg) # find two best matches (closest) for each keypoint
+        matches = bf.match(des_template, des_bkg) # find best match for each keypoint
         matches = sorted(matches, key=lambda x : x.distance) # sort by distance
-        good = matches[:50] # get 50 closest kp
-        res_img = cv2.drawMatches(template_img, kp_template, bkg_img, kp_bkg, good, None, flags = cv2.DRAW_MATCHES_FLAGS_NOT_DRAW_SINGLE_POINTS)
+        good = matches[:10] # get 10 closest kp
+        res_img = cv2.drawMatches(colored_template_img, kp_template, colored_bkg_img, kp_bkg, good, None, flags = cv2.DRAW_MATCHES_FLAGS_NOT_DRAW_SINGLE_POINTS)
 
-    plt.imshow(res_img)
-    plt.show()
+    cv2.imshow("Feature Matching (SIFT)", res_img)
 
 if __name__ == "__main__":
-    template_path = "sprites/baby_alien_img.png"
-    bkg_path = "test.png"
-    bf_matching(template_path, bkg_path, ratio_test = True)
+
+    parser = argparse.ArgumentParser(description="Feature Matching using SIFT for a single template and background image")
+
+    parser.add_argument("--template", type=str, default="sprites/baby_alien_1.png", help="Path to template image")
+    parser.add_argument("--background", type=str, default="background.png", help="Path to background image")
+    parser.add_argument("--ignore_ratio_test", action="store_true", help="Do not use ratio test")
+    args = parser.parse_args()
+    
+    template_path = args.template
+    bkg_path = args.background
+    ratio_test = False if args.ignore_ratio_test else True
+
+    if not Path(template_path).is_file():
+        raise FileNotFoundError(f"'{template_path}' not found")
+    if not Path(bkg_path).is_file():
+        raise FileNotFoundError(f"'{bkg_path}' not found")
+    
+    bf_matching(template_path, bkg_path, ratio_test)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
--- template_matching.py
+++ template_matching.py
@@ -1,43 +1,95 @@
 import cv2
 import numpy as np
+from copy import deepcopy
+from typing import Optional
+from pathlib import Path
+import argparse
 
+""" 
+Insert new matching boxes if they do not intersect with current boxes.
 
-def template_matching(image_path, template_paths, threshold=0.8):
-    image = cv2.imread(image_path)
-    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+"""
+def insert_matching(match_locations : np.ndarray, template_shape : tuple[int,int], matched_regions : Optional[set] = None) -> set:
+    new_matched_regions = deepcopy(matched_regions) if matched_regions != None else set()
+
+    w, h = template_shape
+    for loc in zip(*match_locations[::-1]):
+        x, y = loc
+        region = ((x, y), (x + w, y + h))
+
+        is_duplicate = any(
+            not ((x1 > x + w or x2 < x) or (y1 > y + h or y2 < y))
+            for (x1, y1), (x2, y2) in new_matched_regions
+        )
+        if not is_duplicate:
+            new_matched_regions.add(region)
+    
+    return new_matched_regions
+
+"""
+Calculate and draw matching for a list of template images and a background image
+
+"""
+def template_matching(background_path : str, template_paths : list[str], threshold : float = 0.8) -> None :
+    background = cv2.imread(background_path)
+    
+    templates = [cv2.imread(template_path, 0) for template_path in template_paths]
+    templates += [cv2.flip(template, 1) for template in templates]
+
+    gray_background = cv2.cvtColor(background, cv2.COLOR_BGR2GRAY)
     matched_regions = set()
 
-    for template_path in template_paths:
-        template = cv2.imread(template_path, 0)
-
-        result = cv2.matchTemplate(gray_image, template, cv2.TM_CCOEFF_NORMED)
-        locations = np.where(result >= threshold)
-
-        for loc in zip(*locations[::-1]):
-            x, y = loc
-            w, h = template.shape[::-1]
-            region = (x, y, x + w, y + h)
-
-            is_duplicate = any(
-                not ((x1 > x + w or x2 < x) or (y1 > y + h or y2 < y))
-                for x1, y1, x2, y2 in matched_regions
-            )
-            if not is_duplicate:
-                matched_regions.add(region)
-                cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
+    for template in templates:
+        result = cv2.matchTemplate(gray_background, template, cv2.TM_CCOEFF_NORMED)
+        match_locations = np.where(result >= threshold)
+        matched_regions = insert_matching(match_locations, template.shape[::-1], matched_regions)
 
     print(matched_regions)
-    cv2.imshow("Image with Matches", image)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
 
+    for upleft, downright in matched_regions:
+        cv2.rectangle(background, upleft, downright, (0, 0, 255), 2)
+    
+    cv2.imshow("Template Matching", background)
+
+"""
+Concatenate and print used template images
+
+"""
+def show_templates(template_paths : list[str]) -> None:
+    colored_templates = [cv2.imread(template_path) for template_path in template_paths]
+    max_height = max(template.shape[0] for template in colored_templates)
+    resized_templates = [cv2.resize(template, (int(template.shape[1] * max_height / template.shape[0]), max_height)) for template in colored_templates]
+    combined_templates = np.concatenate(resized_templates, axis=1)
+    cv2.imshow("All templates", combined_templates)
 
 if __name__ == "__main__":
-    template_paths = [
-        "sprites/baby_alien_img.png",
-        "sprites/baby_alien_img2.png",
-    ]
-    image_path = "test.png"
-    threshold = 0.2
 
-    template_matching(image_path, template_paths, threshold)
+    parser = argparse.ArgumentParser(description="Performs mulitple template matching for a set of template images and a background image")
+
+    parser.add_argument("--object", type=str, default=None, help="Name of object to be matched. Only image names containing it a substring will be selected")
+    parser.add_argument("--templates_dir", type=str, default="sprites", help="Name of folder containing template images")
+    parser.add_argument("--background", type=str, default="background.png", help="Path to background image")
+    parser.add_argument("--threshold", type=float, default=0.25, help="Threshold value used in the algorithm")
+
+    args = parser.parse_args()
+
+    object_name = args.object
+    templates_dir = args.templates_dir
+    background_path = args.background
+    threshold = args.threshold
+
+    if object_name != None:
+        template_paths = [str(item) for item in Path(templates_dir).iterdir() if object_name in item.stem]
+    else:
+        template_paths = [str(item) for item in Path(templates_dir).iterdir()]
+
+    # template_paths = [
+    #     # "sprites/baby_alien1.png",
+    #     # "sprites/baby_alien2.png",
+    #     "sprites/baby_alien3.png",
+    # ]
+        
+    show_templates(template_paths)
+    template_matching(background_path, template_paths, threshold)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
